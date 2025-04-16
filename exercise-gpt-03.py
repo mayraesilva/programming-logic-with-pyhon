21ad406f5179a8aeda7fc16d68bd780d8957e7e8
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-16 12:59:15 -0300

    enunciado

---
+++ exercise-gpt-03.py
@@ -0,0 +1,8 @@
+# Crie uma função media(lista) que recebe uma lista de números e retorna a média.
+
+# Crie uma função filtrar_pares(lista) que recebe uma lista e retorna apenas os pares.
+
+# Use a biblioteca random para gerar uma lista com 20 números aleatórios entre 0 e 100 
+# e aplique as duas funções acima.
+
+import random
